fb1abc83439c2c90867dc07a2f3fa3c0834ad020
NeelandsEric <eric.gummerson@neelands.com>
2015-10-29 17:03:38 -0400

    open csv

--- Panel Creator/src/panel/creator/Store.java
+++ Panel Creator/src/panel/creator/Store.java
@@ -1,20 +1,28 @@
 package panel.creator;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Store will contain all information for the store
+ *
  * @author EricGummerson
  */
-public class Store implements java.io.Serializable{
-    
+public class Store implements java.io.Serializable {
+
     private static final long serialVersionUID = 222L;
     public String storeName;
     public String imgStr;
     public int numRacks;
     public ArrayList<Rack> racks;
-    
+
     public Store() {
         this.storeName = "";
         racks = new ArrayList<>();
@@ -33,8 +41,6 @@ public class Store implements java.io.Serializable{
         storeName = "";
     }
 
-    
-    
     public Store(String storeName, String imgStr, int numRacks, ArrayList<Rack> racks) {
         this.storeName = storeName;
         this.imgStr = imgStr;
@@ -69,35 +75,35 @@ public class Store implements java.io.Serializable{
     public ArrayList<Rack> getRacks() {
         return racks;
     }
-    
-    public void setRackIndex(int index, Rack r){
+
+    public void setRackIndex(int index, Rack r) {
         racks.set(index, r);
     }
-    
-    public Rack getRackIndex(int index){
+
+    public Rack getRackIndex(int index) {
         return racks.get(index);
     }
 
     public void setRacks(ArrayList<Rack> racks) {
         this.racks = racks;
     }
-    
-    public void addRack(Rack r){
+
+    public void addRack(Rack r) {
         this.racks.add(r);
     }
-    
-    public String getRackName(int index){
+
+    public String getRackName(int index) {
         return this.racks.get(index).getName();
     }
-    
-    public int getRackNumComp(int index){
+
+    public int getRackNumComp(int index) {
         return this.racks.get(index).getNumCompressors();
     }
-    
-    public int getRackNumFan(int index){
+
+    public int getRackNumFan(int index) {
         return this.racks.get(index).getNumCondenserFans();
     }
-    
+
     /**
      * gets the rack names from the array list
      *
@@ -110,6 +116,37 @@ public class Store implements java.io.Serializable{
         }
         return names;
     }
-    
-    
+
+    public void readCSV(String filepath) {
+
+        try {
+            CSVReader reader = new CSVReader(new FileReader(filepath));
+            String[] nextLine;
+            while ((nextLine = reader.readNext()) != null) {
+                // nextLine[] is an array of values from the line
+                System.out.println(nextLine[0] + nextLine[1] + "etc...");
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Store.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Store.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void writeCSV(String filepath) {
+
+        CSVWriter writer;
+        try {
+            writer = new CSVWriter(new FileWriter("yourfile.csv"), ',');
+            // feed in your array (or convert your data to an array)
+            String[] entries = "first#second#third".split("#");
+            writer.writeNext(entries);
+            writer.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Store.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
 }
