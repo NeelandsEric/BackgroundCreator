e79b14117ea585fed18b3778b563d48a120cb2b6
NeelandsEric <eric.gummerson@neelands.com>
2015-12-09 12:32:07 -0500

    small default text change

--- Panel Creator/src/panel/creator/BackgroundRackNew.java
+++ Panel Creator/src/panel/creator/BackgroundRackNew.java
@@ -3,7 +3,6 @@ package panel.creator;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
-import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
